15b8144e273ece18e48676a59f66eec043579dd0
Jacob <jacobmjones17@gmail.com>
2021-10-11 15:05:32 -0400

    commit tree 2debb4de8d3b2b750e97a907eabb03a2930bddd5

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier() {
+    return function () {
+        return 25
+    }
+}
